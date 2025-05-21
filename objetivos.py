bcb48777bb3d222645ebb76a4bd21f5e6c5c5709
Evelyn Torras <96480092+EvelynMTL@users.noreply.github.com>
2025-05-21 20:03:48 +0000

    borrados certificados y dependencia fisica en objetivos

--- objetivos.py
+++ objetivos.py
@@ -7,7 +7,8 @@ from datetime import datetime, timedelta
 # Verificamos si ya se ha inicializado la app de Firebase
 if not firebase_admin._apps:
     # Si no está inicializada, lo hacemos con las credenciales
-    cred = credentials.Certificate('imagenes_inicio/waving_certificado.json')  # Reemplaza con la ruta de tu archivo de credenciales
+    firebase_dict = st.secrets["firebase"]
+    cred = credentials.Certificate(firebase_dict)
     firebase_admin.initialize_app(cred)
 else:
     # Si ya está inicializada, no la volvemos a inicializar
