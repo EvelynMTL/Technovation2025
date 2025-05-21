f0f3873a52491df62afe55acd21ad756b61b4a60
Evelyn Torras <96480092+EvelynMTL@users.noreply.github.com>
2025-05-21 19:46:37 +0000

    quitar dependencia fisica de certificado

--- perfil.py
+++ perfil.py
@@ -7,8 +7,11 @@ try:
     # Verificamos si ya se ha inicializado la app de Firebase
     if not firebase_admin._apps:
         # Si no está inicializada, lo hacemos con las credenciales
-        cred = credentials.Certificate('imagenes_inicio/waving_certificado.json')  # Reemplaza con la ruta de tu archivo de credenciales
+        firebase_dict = st.secrets["firebase"]
+        cred = credentials.Certificate(firebase_dict)
+        #cred = credentials.Certificate('imagenes_inicio/waving_certificado.json')  # Reemplaza con la ruta de tu archivo de credenciales
         firebase_admin.initialize_app(cred)
+  
     else:
         # Si ya está inicializada, no la volvemos a inicializar
         app = firebase_admin.get_app()
